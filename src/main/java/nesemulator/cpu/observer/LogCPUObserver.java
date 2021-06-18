a7553ca735102c1839f0287cd488ba3631ae8cc7
Luiz Carlos Cavalcanti <cavalcanti.luiz@gmail.com>
2021-06-18 20:13:18 -0300

    [logger] proper hex formating for 8 and 16-bit values
    Signed-off-by: Luiz Carlos Cavalcanti <cavalcanti.luiz@gmail.com>

--- src/main/java/nesemulator/cpu/observer/LogCPUObserver.java
+++ src/main/java/nesemulator/cpu/observer/LogCPUObserver.java
@@ -21,19 +21,19 @@ public class LogCPUObserver implements CPUObserver {
                 return "";
             case RELATIVE:
             case ZERO_PAGE:
-                return String.format(" $%02X", operands[0]);
+                return String.format(" $%02X", operands[0] & 0xFF);
             case IMMEDIATE:
-                return String.format(" #$%02X", operands[0]);
+                return String.format(" #$%02X", operands[0] & 0xFF);
             case ABSOLUTE:
-                return String.format(" $%04X", operands[0]);
+                return String.format(" $%04X", operands[0] & 0xFFFF);
             case ZERO_PAGE_X:
-                return String.format(" $%02X, X", operands[0]);
+                return String.format(" $%02X, X", operands[0] & 0xFF);
             case ABSOLUTE_X:
-                return String.format(" $%04X, X", operands[0]);
+                return String.format(" $%04X, X", operands[0] & 0xFFFF);
             case ABSOLUTE_Y:
-                return String.format(" $%04X, Y", operands[0]);
+                return String.format(" $%04X, Y", operands[0] & 0xFFFF);
             case INDIRECT_Y:
-                return String.format(" ($%04X), Y", operands[0]);
+                return String.format(" ($%04X), Y", operands[0] & 0xFFFF);
             default:
                 throw new UnsupportedOperationException("Please write a log handler for this addressing mode: " + addressingMode.name());
         }
