db4a1b4b24f1cce8b300c6ac57146223e5cc3a8e
Luiz Carlos Cavalcanti <cavalcanti.luiz@gmail.com>
2021-06-20 21:35:53 -0300

    [inspect] add control and mask info on PPU panel
    Signed-off-by: Luiz Carlos Cavalcanti <cavalcanti.luiz@gmail.com>

--- src/main/java/nesemulator/ui/PPUPane.java
+++ src/main/java/nesemulator/ui/PPUPane.java
@@ -7,6 +7,8 @@ import nesemulator.PPU;
 public class PPUPane extends VBox {
 
     private final Label statusLabel;
+    private final Label controlLabel;
+    private final Label maskLabel;
 
     public PPUPane() {
         statusLabel = new Label("Status: --------");
@@ -15,18 +17,36 @@ public class PPUPane extends VBox {
         var statusHelp = new Label("        VSO-----");
         statusHelp.getStyleClass().addAll("register-value");
 
+        controlLabel = new Label("Control: --------");
+        controlLabel.getStyleClass().add("register-value");
+
+        var controlHelp = new Label("         VPHBSINN");
+        controlHelp.getStyleClass().addAll("register-value");
+
+        maskLabel = new Label("Mask: --------");
+        maskLabel.getStyleClass().add("register-value");
+
+        var maskHelp = new Label("      BGRsbMmG");
+        maskHelp.getStyleClass().addAll("register-value");
+
         this.getChildren().addAll(
                 new Label("PPU State:"),
+                controlLabel,
+                controlHelp,
+                maskLabel,
+                maskHelp,
                 statusLabel,
                 statusHelp
         );
     }
 
     public void update() {
-        statusLabel.setText(String.format("Status: %s", intToByteBinary(PPU.inspect(0x2002))));
+        controlLabel.setText(String.format("Control: %s", toBin(PPU.inspect(PPU.ADDRESS_PPUCTRL))));
+        maskLabel.setText(String.format("Mask: %s", toBin(PPU.inspect(PPU.ADDRESS_PPUMASK))));
+        statusLabel.setText(String.format("Status: %s", toBin(PPU.inspect(PPU.ADDRESS_PPUSTATUS))));
     }
 
-    private String intToByteBinary(int value) {
+    private String toBin(int value) {
         return String.format("%8s", Integer.toBinaryString(value & 0xFF)).replace(' ', '0');
     }
 
